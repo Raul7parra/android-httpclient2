6416fb675b0f1647db70abe82a1d25c8e7d8f97e
Raul <raul7parrahernandez@gmail.com>
2026-01-08 13:20:59 +0100

    Apartado 3 hecho en clase

---
+++ app/src/main/java/es/fpsumma/dam2/api/model/Tarea.kt
@@ -0,0 +1,9 @@
+package es.fpsumma.dam2.api.model
+
+import android.accessibilityservice.GestureDescription
+
+data class Tarea(
+    val id: Int,
+    val titulo: String,
+    val description: String
+)
--- app/src/main/java/es/fpsumma/dam2/api/viewmodel/TareasViewModel.kt
+++ app/src/main/java/es/fpsumma/dam2/api/viewmodel/TareasViewModel.kt
@@ -6,8 +6,11 @@ import androidx.lifecycle.viewModelScope
 import androidx.room.Room
 import es.fpsumma.dam2.api.data.local.AppDatabase
 import es.fpsumma.dam2.api.data.local.entity.TareaEntity
+import es.fpsumma.dam2.api.model.Tarea
+import es.fpsumma.dam2.api.ui.screen.tareas.TareasUIState
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
@@ -19,9 +22,13 @@ class TareasViewModel(app: Application) : AndroidViewModel(app) {
 
     private val dao = db.tareaDao()
 
-    val tareas: StateFlow<List<TareaEntity>> =
-        dao.getAllTareas().stateIn(viewModelScope, SharingStarted.Lazily, emptyList())
-
+    val state: StateFlow<TareasUIState> =
+        dao.getAllTareas()
+            .map { lista ->
+                TareasUIState(
+                    tareas = lista.map { Tarea(it.id, it.titulo, it.descripcion) }
+                )}
+            .stateIn(viewModelScope, SharingStarted.Lazily, TareasUIState())
     fun getTarea(id: Int): StateFlow<TareaEntity?> =
         dao.getTarea(id).stateIn(viewModelScope, SharingStarted.WhileSubscribed(5_000), null)
 
