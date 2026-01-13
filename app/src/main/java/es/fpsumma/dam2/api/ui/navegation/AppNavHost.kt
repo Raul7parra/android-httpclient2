af6a23a9a4ab3e64ff982890743bd46c9984317e
Raul <raul7parrahernandez@gmail.com>
2026-01-13 17:16:39 +0100

    Parte 3 hecha en casa

--- app/src/main/java/es/fpsumma/dam2/api/ui/navegation/AppNavHost.kt
+++ app/src/main/java/es/fpsumma/dam2/api/ui/navegation/AppNavHost.kt
@@ -8,15 +8,25 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import es.fpsumma.dam2.api.ui.screen.tareas.DetalleTareaRoomRoute
 import es.fpsumma.dam2.api.ui.screen.tareas.DetalleTareaScreen
+import es.fpsumma.dam2.api.ui.screen.tareas.ListadoTareasRemoteRoute
 import es.fpsumma.dam2.api.ui.screen.tareas.ListadoTareasRoomRoute
 import es.fpsumma.dam2.api.ui.screen.tareas.ListadoTareasScreen
 import es.fpsumma.dam2.api.ui.screen.tareas.NuevaTareaRoomRoute
 import es.fpsumma.dam2.api.ui.screen.tareas.NuevaTareaScreen
+import es.fpsumma.dam2.api.viewmodel.TareasRemoteViewModel
 import es.fpsumma.dam2.api.viewmodel.TareasViewModel
 
 @Composable
-fun AppNavHost(navController: NavHostController, tareasViewModel: TareasViewModel) {
-    NavHost(navController = navController, startDestination = Routes.TAREA_LISTADO) {
+fun AppNavHost(
+    navController: NavHostController,
+    tareasViewModel: TareasViewModel,
+    tareasRemoteViewModel: TareasRemoteViewModel //añadimos este ViewModel para añadir la ruta de la API con la screen ListadoTareasRemoteRoute
+)
+{
+    //Cambiamos el startDestination por TAREA_LISTADO_API
+    NavHost(navController = navController, startDestination = Routes.TAREA_LISTADO_API){
+        //Añadimos la ruta de la API con la screen que hemos creado anteriormente
+        composable(Routes.TAREA_LISTADO_API) { ListadoTareasRemoteRoute(navController, tareasRemoteViewModel) }
         composable(Routes.TAREA_LISTADO) { ListadoTareasRoomRoute(navController, tareasViewModel) }
         composable(Routes.TAREA_ADD) { NuevaTareaRoomRoute(navController, tareasViewModel) }
         composable(
---
+++ app/src/main/java/es/fpsumma/dam2/api/ui/screen/tareas/ListadoTareasRemoteRoute.kt
@@ -0,0 +1,30 @@
+package es.fpsumma.dam2.api.ui.screen.tareas
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavController
+import es.fpsumma.dam2.api.ui.navegation.Routes
+import es.fpsumma.dam2.api.viewmodel.TareasRemoteViewModel
+import es.fpsumma.dam2.api.viewmodel.TareasViewModel
+
+@Composable
+fun ListadoTareasRemoteRoute(
+    navController: NavController,
+    vm: TareasRemoteViewModel,
+){
+    val state by vm.state.collectAsState()
+
+    LaunchedEffect(Unit) {
+        vm.loadTareas()
+    }
+
+    ListadoTareasContent(
+        state = state,
+        onBack = { navController.popBackStack() },
+        onAdd = { navController.navigate(Routes.TAREA_ADD) },
+        onOpenDetalle = { id -> navController.navigate(Routes.tareaView(id)) },
+        onDelete = {},
+    )
+}
